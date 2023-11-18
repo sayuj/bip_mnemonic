3ee540efce865118df7c6e956a54b4eb654f0d5c
Adão Raul <adao.raul@gmail.com>
2023-11-18 18:46:58 -0300

    main class refactored tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'simplecov'
 SimpleCov.start
-Dir[File.join(File.dirname(__FILE__), '..', "lib", "*.rb")].each { |f| require f }
+Dir[File.join(File.dirname(__FILE__), '..', 'lib', '*.rb')].sort.each { |f| require f }
